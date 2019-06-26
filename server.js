fb10cf022be5f962efaf790c23c8e2a713295ffe
GitGudJavaScript <gitgudcasul@pm.me>
2019-06-26 17:33:39 +0100

    Add authentication middleware

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect("mongodb://localhost:27017/ohaiDB", {useNewUrlParser: true});
 
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, User, bcrypt); })
 app.post('/register', (req, res) => { register.handleRegister(req, res, User, bcrypt); })
-app.get('/placeholder/:id', (req, res) => { return res.json("Logged in and session authenticated")})
+app.get('/placeholder/:id', auth.isAuthenticated, (req, res) => { return res.json("Logged in and session authenticated")})
 
 app.listen(3000, (req, res) => {
   console.log("Server running on port 3000");
