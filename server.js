e83157a32fcd830bc7f73329f45d025e6311179a
GitGudJavaScript <gitgudcasul@pm.me>
2019-06-27 14:07:56 +0100

    Added signout route

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const signout = require('./controllers/signout');
 const auth = require('./controllers/authorization');
 
 const userSchema = new mongoose.Schema({ email: String, hash: String })
@@ -22,8 +23,8 @@ mongoose.connect("mongodb://localhost:27017/ohaiDB", {useNewUrlParser: true});
 
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, User, bcrypt); })
 app.post('/register', (req, res) => { register.handleRegister(req, res, User, bcrypt); })
-app.get('/placeholder', auth.isAuthenticated, (req, res) => res.json("You are still authenticated"))
 app.get('/placeholder/:id', auth.isAuthenticated, (req, res) => { return res.json("Logged in and session authenticated")})
+app.get('/signout', (req, res) => { signout.handleSignOut(req, res) })
 
 app.listen(3000, (req, res) => {
   console.log("Server running on port 3000");
