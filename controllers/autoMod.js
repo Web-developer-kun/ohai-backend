e02ce37dc29dce01f09ddb49fc50606ce66d139c
GitGudJavaScript <gitgudcasul@pm.me>
2019-07-06 11:00:15 +0100

    Set up face detection api

---
+++ controllers/autoMod.js
@@ -0,0 +1,20 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: process.env.CLARIFAI_API
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.url)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err =>
+      res.status(400).json("Something went wrong with Clarifai API")
+    );
+};
+
+module.exports = {
+  handleApiCall
+};
