5be394aa976f28acbe6e2d1a93ee58022e38acc9
GitGudJavaScript <gitgudcasul@pm.me>
2019-07-10 12:51:45 +0100

    Finish backend refactor

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const handleSendReceiveMsgPost = (msg, io, TsqPost) => {
   const newTsqPost = new TsqPost({
     user: msg.user,
@@ -16,6 +18,40 @@ const handleSendReceiveMsgPost = (msg, io, TsqPost) => {
   });
 };
 
+const handleSendReceivePvtMsg = (pvtMsg, io, socket, TsqPost) => {
+  const newTsqPost = new TsqPost({
+    user: pvtMsg.user,
+    message: pvtMsg.message,
+    src: pvtMsg.src,
+    sid: pvtMsg.sid,
+    time: pvtMsg.time
+  });
+
+  Promise.resolve(
+    newTsqPost.save(err => {
+      if (err) return res.json(err);
+    })
+  ).then(() => {
+    io.to(newTsqPost.sid).emit("receive-private-message", newTsqPost);
+    io.to(socket.id).emit("receive-private-message", newTsqPost);
+  });
+};
+
+const handleNewUser = (username, io, socket) => {
+  socket.username = username;
+  const onlineSIDs = [];
+  _.forIn(io.sockets.sockets, (value, key) => {
+    let sid = {
+      sid: key,
+      username: io.sockets.sockets[key].username
+    };
+    onlineSIDs.push(sid);
+  });
+  io.emit("receive-connected-sockets", onlineSIDs);
+};
+
 module.exports = {
-  handleSendReceiveMsgPost
+  handleSendReceiveMsgPost,
+  handleSendReceivePvtMsg,
+  handleNewUser
 };
